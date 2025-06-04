9f5e9d6c6ed9353a989c1904202b1427acc7dbae
Vladislav Flinski <flinssski@gmail.com>
2025-06-04 16:07:37 +0300

    feat: add search bar

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,15 @@
+import SearchIcon from "@/components/icons/SearchIcon"
+import styles from "./SearchBar.module.scss"
+
+export default function SearchBar() {
+	return (
+		<div className={styles.searchBar}>
+			<div className={styles.searchBarWrapper}>
+				<button>
+					<SearchIcon />
+				</button>
+				<input type="text" placeholder="Search music..." />
+			</div>
+		</div>
+	)
+}
